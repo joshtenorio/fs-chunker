99c91d93c2be999e7898e5493b845871e0f56731
Joshua Tenorio <joshliam09@gmail.com>
2024-04-10 01:20:45 -0700

    feat: compute a chunk's sha256 hash

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,20 @@
 use std::{io::Read, path::Path};
+use sha256::digest;
+pub struct Chunk {
+    data: Vec<u8>,
+    hash: String
+}
 
-pub fn chunk_file<P>(path: P, chunk_size: usize) -> Vec<Vec<u8>> where P: AsRef<Path> {
+pub fn chunk_file<P>(path: P, chunk_size: usize) -> Vec<Chunk> where P: AsRef<Path> {
     let mut file = std::fs::File::open(path).unwrap();
-    let mut output = Vec::new();
+    let mut output: Vec<Chunk> = Vec::new();
 
     loop {
         let mut chunk = Vec::with_capacity(chunk_size);
         let n = file.by_ref().take(chunk_size as u64).read_to_end(&mut chunk).unwrap();
         if n == 0 { break; } // nothing read
-        output.push(chunk);
+        let sha256_hash = digest(chunk.clone());
+        output.push(Chunk { data: chunk, hash: sha256_hash });
         if n < chunk_size { break; } // if we read less bytes than chunk size, exit loop too
     }
 
