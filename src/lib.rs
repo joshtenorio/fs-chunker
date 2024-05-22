4b76d7e929ccc30fac6eee4eb4f198d714e259ce
Joshua Tenorio <joshliam09@gmail.com>
2024-05-21 23:59:11 -0700

    feat: add option to use blake3 instead of sha256

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ pub struct Chunk {
     pub hash: String
 }
 
-pub fn chunk_file<P>(path: P, chunk_size: usize) -> Vec<Chunk> where P: AsRef<Path> {
+pub fn chunk_file<P>(path: P, chunk_size: usize, use_blake: bool) -> Vec<Chunk> where P: AsRef<Path> {
     let mut file = std::fs::File::open(path).unwrap();
     let mut output: Vec<Chunk> = Vec::new();
 
@@ -13,15 +13,21 @@ pub fn chunk_file<P>(path: P, chunk_size: usize) -> Vec<Chunk> where P: AsRef<Pa
         let mut chunk: Vec<u8> = Vec::with_capacity(chunk_size);
         let n = file.by_ref().take(chunk_size as u64).read_to_end(&mut chunk).unwrap();
         if n == 0 { break; } // nothing read
-        let sha256_hash = digest(chunk.clone());
-        output.push(Chunk { data: chunk, hash: sha256_hash });
+        let hash;
+        if use_blake {
+            hash = blake3::hash(&chunk).to_string();
+        }
+        else {
+            hash = digest(chunk.clone());
+        }
+        output.push(Chunk { data: chunk, hash });
         if n < chunk_size { break; } // if we read less bytes than chunk size, exit loop too
     }
 
     output
 }
 
-pub fn hash_file<P>(path: P, chunk_size: usize) -> Vec<String> where P: AsRef<Path> {
+pub fn hash_file<P>(path: P, chunk_size: usize, use_blake: bool) -> Vec<String> where P: AsRef<Path> {
     let mut file = std::fs::File::open(path).unwrap();
     let mut output: Vec<String> = Vec::new();
 
@@ -29,8 +35,14 @@ pub fn hash_file<P>(path: P, chunk_size: usize) -> Vec<String> where P: AsRef<Pa
         let mut chunk: Vec<u8> = Vec::with_capacity(chunk_size);
         let n = file.by_ref().take(chunk_size as u64).read_to_end(&mut chunk).unwrap();
         if n == 0 { break; } // nothing read
-        let sha256_hash = digest(chunk.clone());
-        output.push(sha256_hash);
+        let hash: String;
+        if use_blake {
+            hash = blake3::hash(&chunk).to_string();
+        }
+        else {
+            hash = digest(chunk.clone());
+        }
+        output.push(hash);
         if n < chunk_size { break; } // if we read less bytes than chunk size, exit loop too
     }
 
